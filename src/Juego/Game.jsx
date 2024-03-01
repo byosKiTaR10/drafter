ca6e98c52c731870f257b22b4f4d2f31012813f7
byosKiTaR10 <os-kita@hotmail.es>
2024-03-01 20:09:06 +0000

    Correcciones CSS

--- src/Juego/Game.jsx
+++ src/Juego/Game.jsx
@@ -24,82 +24,81 @@ const Game = () => {
   };
 
   return (
-    <div>
+    <>
       <h1>Champ Select</h1>
       <div className="content">
-        <div className="container-team1">
-          <div className="team1-grid">
-            <div className="teams-item">
-              <div className="teams-card">
-                <img src={`data:image/jpeg;base64,${champions[0].img}`} alt={champions[0].name} />
-              </div>
-              <text>Hola</text>
+
+        <div className="team1-grid">
+          <div className="teams-item">
+            <div className="teams-card">
+              <img src={`data:image/jpeg;base64,${champions[0].img}`} alt={champions[0].name} />
             </div>
-            <div className="teams-item">
-              <div className="teams-card">
-                <img src={`data:image/jpeg;base64,${champions[1].img}`} alt={champions[1].name} />
-              </div>
-              <text>Hola</text>
+            <text>Hola</text>
+          </div>
+          <div className="teams-item">
+            <div className="teams-card">
+              <img src={`data:image/jpeg;base64,${champions[1].img}`} alt={champions[1].name} />
             </div>
-            <div className="teams-item">
-              <div className="teams-card">
-                <img src={`data:image/jpeg;base64,${champions[2].img}`} alt={champions[2].name} />
-              </div>
-              <text>Hola</text>
+            <text>Hola</text>
+          </div>
+          <div className="teams-item">
+            <div className="teams-card">
+              <img src={`data:image/jpeg;base64,${champions[2].img}`} alt={champions[2].name} />
             </div>
-            <div className="teams-item">
-              <div className="teams-card">
-                <img src={`data:image/jpeg;base64,${champions[3].img}`} alt={champions[3].name} />
-              </div>
-              <text>Hola</text>
+            <text>Hola</text>
+          </div>
+          <div className="teams-item">
+            <div className="teams-card">
+              <img src={`data:image/jpeg;base64,${champions[3].img}`} alt={champions[3].name} />
             </div>
-            <div className="teams-item">
-              <div className="teams-card">
-                <img src={`data:image/jpeg;base64,${champions[4].img}`} alt={champions[4].name} />
-              </div>
-              <text>Hola</text>
+            <text>Hola</text>
+          </div>
+          <div className="teams-item">
+            <div className="teams-card">
+              <img src={`data:image/jpeg;base64,${champions[4].img}`} alt={champions[4].name} />
+            </div>
+            <text>Hola</text>
+          </div>
+        </div>
+        <div className="champion-grid">
+          {champions.map((champion, index) => (
+            <div key={index} className="champion-card">
+              <img src={`data:image/jpeg;base64,${champion.img}`} alt={champion.name} onClick={() => handleChampionClick(champion)} />
+              <p>{champion.name}</p>
+            </div>
+          ))}
+        </div>
+
+        <div className="team2-grid">
+          <div className="teams-item">
+            <div className="teams-card">
+              <img src={`data:image/jpeg;base64,${champions[0].img}`} alt={champions[0].name} />
+            </div>
+          </div>
+          <div className="teams-item">
+            <div className="teams-card">
+              <img src={`data:image/jpeg;base64,${champions[1].img}`} alt={champions[1].name} />
+            </div>
+          </div>
+          <div className="teams-item">
+            <div className="teams-card">
+              <img src={`data:image/jpeg;base64,${champions[2].img}`} alt={champions[2].name} />
+            </div>
+          </div>
+          <div className="teams-item">
+            <div className="teams-card">
+              <img src={`data:image/jpeg;base64,${champions[3].img}`} alt={champions[3].name} />
+            </div>
+          </div>
+          <div className="teams-item">
+            <div className="teams-card">
+              <img src={`data:image/jpeg;base64,${champions[4].img}`} alt={champions[4].name} />
             </div>
           </div>
         </div>
-        <div className="container-team2">
-          <div className="team2-grid">
-            <div className="teams-item">
-              <div className="teams-card">
-                <img src={`data:image/jpeg;base64,${champions[0].img}`} alt={champions[0].name} />
-              </div>
-            </div>
-            <div className="teams-item">
-              <div className="teams-card">
-                <img src={`data:image/jpeg;base64,${champions[1].img}`} alt={champions[1].name} />
-              </div>
-            </div>
-            <div className="teams-item">
-              <div className="teams-card">
-                <img src={`data:image/jpeg;base64,${champions[2].img}`} alt={champions[2].name} />
-              </div>
-            </div>
-            <div className="teams-item">
-              <div className="teams-card">
-                <img src={`data:image/jpeg;base64,${champions[3].img}`} alt={champions[3].name} />
-              </div>
-            </div>
-            <div className="teams-item">
-              <div className="teams-card">
-                <img src={`data:image/jpeg;base64,${champions[4].img}`} alt={champions[4].name} />
-              </div>
-            </div>
-          </div>
-        </div>
-          <div className="champion-grid">
-            {champions.map((champion, index) => (
-              <div key={index} className="champion-card">
-                <img src={`data:image/jpeg;base64,${champion.img}`} alt={champion.name} onClick={() => handleChampionClick(champion)} />
-                <p>{champion.name}</p>
-              </div>
-            ))}
-          </div>
+
       </div>
-    </div>
+    </>
   );
 }
 export default Game;
